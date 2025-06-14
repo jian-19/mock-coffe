bbc1c8436fc28401d2cb107e650bce25313917ed
jian-19 <jian-ferreira@hotmail.com>
2025-06-14 19:16:11 -0300

    add image products

--- models/products.js
+++ models/products.js
@@ -2,7 +2,7 @@ import database from "@/infra/database";
 
 async function getAllProductsByCategory(categoryId) {
   return await database.query(
-    "SELECT p.title, p.content, p.amount, p.image FROM products p WHERE p.categoryid = $1;",
+    "SELECT p.title, p.content, p.amount, p.image FROM products p WHERE p.category_id = $1;",
     [categoryId]
   );
 }
